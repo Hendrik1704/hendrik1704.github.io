5f80d51a7e3fe755dbd269ec09f7eb2266f55b18
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2025-01-22 18:11:23 -0500

    First version

---
+++ scripts/nav.js
@@ -0,0 +1,11 @@
+// This script inserts the navigation bar near the top of
+// every page.
+
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#insert_menu");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
---
+++ scripts/slides.js
@@ -0,0 +1,17 @@
+// This script is used to cycle through the images that
+// are displayed on the main index.html
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // advance image every 2 seconds
+}
